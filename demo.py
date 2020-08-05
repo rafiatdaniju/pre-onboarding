8e0b15e2025380435d034cde34daaeff2565493c
Rafiat <danijurafiat@gmail.com>
2020-08-05 11:59:44 +0100

    Add demo.py file

---
+++ demo.py
@@ -0,0 +1,11 @@
+def isum(a, b):
+    return a + b
+
+isum_result = isum(5, 10)
+
+class DemoClass():
+    def __init__(self, name):
+        self.name = name
+ 
+    def getName(self):
+        return self.name
